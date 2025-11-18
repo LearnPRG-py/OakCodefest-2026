93ec237e440bb4e321e118c7a7be3f3adef9f120
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-11-18 16:13:34 +0530

    Finish contact section

---
+++ ContactUsSection/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fields = document.querySelectorAll(".field");
+
+    // Create ONE global cursor
+    const cursor = document.createElement("span");
+    cursor.classList.add("cursor");
+    cursor.textContent = "▌";
+    cursor.style.fontSize = "20px";
+    cursor.style.position = "fixed";
+    document.body.appendChild(cursor);
+
+    let delay = 0;
+
+    fields.forEach(field => {
+        const text = field.textContent.trim();
+        field.textContent = "";
+
+        const span = document.createElement("span");
+        field.appendChild(span);
+
+        const colonIndex = text.indexOf(":");
+
+        [...text].forEach((char, i) => {
+            setTimeout(() => {
+                if (i <= colonIndex) {
+                    span.innerHTML += `<span class="label">${char}</span>`;
+                } else {
+                    span.innerHTML += char;
+                }
+                const rect = field.getBoundingClientRect();
+                cursor.style.left = (rect.left + span.offsetWidth + 6) + "px";
+                cursor.style.top = rect.top + "px";
+
+            }, delay + i * 30);
+        });
+
+        delay += text.length * 30 + 200;
+    });
+});
