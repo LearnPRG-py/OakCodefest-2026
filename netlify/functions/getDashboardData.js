5cc25a326cc187cf8715428193ddd630a41bf76a
Sreshty-Beshty <126054700+Sreshty-Beshty@users.noreply.github.com>
2026-01-24 10:16:02 +0530

    Update getDashboardData.js

--- netlify/functions/getDashboardData.js
+++ netlify/functions/getDashboardData.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://frakealogxcmmcoayisl.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyYWtlYWxvZ3hjbW1jb2F5aXNsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2OTIwMTM3MywiZXhwIjoyMDg0Nzc3MzczfQ.pF2G9pNXo9gXZNYG0-wHB_qp9o3uwd0fEvqbyxGhjjQ"
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export const handler = async (event) => {
